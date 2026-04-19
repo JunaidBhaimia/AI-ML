6081e5f792833afb077d396538a7ce6701af3f6f
JunaidBhaimia <junaid.pvt.2005@gmail.com>
2026-04-19 13:35:41 +0530

    Add Naive Bayes implementation for Iris dataset

---
+++ LAB-Experiments/ML/SupervisedLearning/NavieBayes.py
@@ -0,0 +1,24 @@
+#NAIVE BAYES
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = GaussianNB()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average='macro')
+recall = recall_score(y_test, y_pred, average='macro')
+f1 = f1_score(y_test, y_pred, average='macro')
+
+print("Accuracy :", accuracy)
+print("Precision:", precision)
+print("Recall   :", recall)
+print("F1 Score :", f1)
