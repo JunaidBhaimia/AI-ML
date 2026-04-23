4c93bd49905dc6f895bb706a98f02fffbfb1cffd
JunaidBhaimia <junaid.pvt.2005@gmail.com>
2026-04-23 18:49:02 +0530

    Update SmartCities.py

--- Assignment/SmartCities.py
+++ Assignment/SmartCities.py
@@ -1,1 +1,28 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
+df = pd.read_csv("traffic.csv")
+
+df.columns = df.columns.str.strip().str.lower()
+
+print("Columns:", df.columns)
+
+X = df[['carcount', 'bikecount', 'buscount', 'truckcount']]
+
+y = df['total']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("R2 Score:", r2_score(y_test, y_pred))
+print("MSE:", mean_squared_error(y_test, y_pred))
+
+print("Prediction:", model.predict([[50, 30, 10, 5]])[0])
